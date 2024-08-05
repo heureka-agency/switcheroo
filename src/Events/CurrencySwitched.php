c1419c6dda17d95010d7a55060ee6962605695bf
Tristan Thar <thar.tristan@heureka-kreativ.hu>
2024-08-05 23:26:36 +0200

    - add the current user as a parameter to CurrencySwitched

--- src/Events/CurrencySwitched.php
+++ src/Events/CurrencySwitched.php
@@ -2,6 +2,7 @@
 
 namespace HeurekaAgency\Switcheroo\Events;
 
+use App\Models\User;
 use HeurekaAgency\Switcheroo\Enums\Currency;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
@@ -14,23 +15,25 @@ class CurrencySwitched implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public User $user;
+
     public function __construct(
         public Currency $currency,
         protected ?bool $shouldBroadCast = null,
         protected ?bool $privateChannel = null
-    ) {}
+    ) {
+        $this->user = auth()->user();
+    }
 
     public function broadcastOn(): array
     {
         $shouldBroadcast = $this->shouldBroadCast ?? config('switcheroo.events.broadcast');
         $privateChannel = $this->privateChannel ?? config('switcheroo.events.private');
 
-        $user = auth()->user();
-
         if ($shouldBroadcast) {
             return [
                 $privateChannel
-                    ? new PrivateChannel("switcheroo.{$user->id}")
+                    ? new PrivateChannel("switcheroo.{$this->user->id}")
                     : new Channel('switcheroo')
             ];
         }
